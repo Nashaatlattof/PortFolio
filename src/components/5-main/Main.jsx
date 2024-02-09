a056ef46f80f9e638b48053dbc5bae12149963df
Your Name <you@example.com>
2024-02-10 00:46:10 +0300

    first commit1

--- src/components/5-main/Main.jsx
+++ src/components/5-main/Main.jsx
@@ -1,59 +1,62 @@
 import { useState } from "react";
 import "./main.css";
 import { AnimatePresence, motion } from "framer-motion";
-const myProjects = [
-  {
-    projectTiltle: "Admin Dashboard Ecommerce",
-    category: ["React", "Javascript"],
-    imgPath: "src/assets/imgs/Capture10.PNG",
-    repoPath: "https://github.com/Nashaatlattof/Admin-Dashboard-Ecommerce.git",
-    demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
-  },
-  {
-    projectTiltle: "Ecommerce Website",
-    category: ["React", "MUI", "Javascript"],
-    imgPath: "src/assets/imgs/Capture20.PNG",
-    repoPath: "https://github.com/Nashaatlattof/E-commerce-Website-Project.git",
-    demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
-  },
-  {
-    projectTiltle: "Portfolio Template",
-    category: ["Javascript", "Bootstrap"],
-    imgPath: "src/assets/imgs/Capture3.PNG",
-    repoPath: "https://github.com/Nashaatlattof/Portfolio-Template.git",
-    demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
-  },
-  {
-    projectTiltle: "Elzero template3",
-    category: ["Html/Css", "Javascript"],
-    imgPath: "src/assets/imgs/Capture1.PNG",
-    repoPath: "https://github.com/Nashaatlattof/projrct-Html-Css-Js.git",
-    demoPath: "https://nashaatlattof.github.io/projrct-Html-Css-Js/",
-  },
-  {
-    projectTiltle: "static website",
-    category: ["Bootstrap"],
-    imgPath: "src/assets/imgs/Capture.PNG",
-    repoPath: "https://github.com/Nashaatlattof/Project-Bootstrap5.git",
-    demoPath: "https://nashaatlattof.github.io/Project-Bootstrap5/",
-  },
-  {
-    projectTiltle: "static website",
-    category: ["Bootstrap"],
-    imgPath: "src/assets/imgs/Capture2.PNG",
-    repoPath: "https://github.com/Nashaatlattof/Bondi-Project.git",
-    demoPath: "https://nashaatlattof.github.io/Bondi-Project/",
-  },
-  {
-    projectTiltle: "static website",
-    category: ["Html/Css"],
-    imgPath: "src/assets/imgs/5.jpg",
-    repoPath: "https://github.com/Nashaatlattof/Project-Html-Css.git",
-    demoPath: "https://nashaatlattof.github.io/Project-Html-Css/",
-  },
-];
+
 
 export default function Main() {
+  const myProjects = [
+    {
+      projectTiltle: "Admin Dashboard Ecommerce",
+      category: ["React", "Javascript"],
+      imgPath: "src/assets/imgs/Capture10.PNG",
+      repoPath:
+        "https://github.com/Nashaatlattof/Admin-Dashboard-Ecommerce.git",
+      demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
+    },
+    {
+      projectTiltle: "Ecommerce Website",
+      category: ["React", "MUI", "Javascript"],
+      imgPath: "src/assets/imgs/Capture20.PNG",
+      repoPath:
+        "https://github.com/Nashaatlattof/E-commerce-Website-Project.git",
+      demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
+    },
+    {
+      projectTiltle: "Portfolio Template",
+      category: ["Javascript", "Bootstrap"],
+      imgPath: "src/assets/imgs/Capture3.PNG",
+      repoPath: "https://github.com/Nashaatlattof/Portfolio-Template.git",
+      demoPath: "https://nashaatlattof.github.io/Portfolio-Template/",
+    },
+    {
+      projectTiltle: "Elzero template3",
+      category: ["Html/Css", "Javascript"],
+      imgPath: "src/assets/imgs/Capture1.PNG",
+      repoPath: "https://github.com/Nashaatlattof/projrct-Html-Css-Js.git",
+      demoPath: "https://nashaatlattof.github.io/projrct-Html-Css-Js/",
+    },
+    {
+      projectTiltle: "static website",
+      category: ["Bootstrap"],
+      imgPath: "src/assets/imgs/Capture.PNG",
+      repoPath: "https://github.com/Nashaatlattof/Project-Bootstrap5.git",
+      demoPath: "https://nashaatlattof.github.io/Project-Bootstrap5/",
+    },
+    {
+      projectTiltle: "static website",
+      category: ["Bootstrap"],
+      imgPath: "src/assets/imgs/Capture2.PNG",
+      repoPath: "https://github.com/Nashaatlattof/Bondi-Project.git",
+      demoPath: "https://nashaatlattof.github.io/Bondi-Project/",
+    },
+    {
+      projectTiltle: "static website",
+      category: ["Html/Css"],
+      imgPath: "src/assets/imgs/5.jpg",
+      repoPath: "https://github.com/Nashaatlattof/Project-Html-Css.git",
+      demoPath: "https://nashaatlattof.github.io/Project-Html-Css/",
+    },
+  ];
   const [currentActive, setCurrentActive] = useState("All Projects");
   const [projects, setProjects] = useState(myProjects);
 
